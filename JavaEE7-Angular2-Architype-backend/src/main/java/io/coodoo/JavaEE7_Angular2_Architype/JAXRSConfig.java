dd889414ff02d06eb7d00c1a02e6c141d936a182
JUAN PABLO <JUGIRALDO1@GMAIL.COM>
2017-09-24 20:14:37 -0500

    //actualizacion

---
+++ JavaEE7-Angular2-Architype-backend/src/main/java/io/coodoo/JavaEE7_Angular2_Architype/JAXRSConfig.java
@@ -0,0 +1,9 @@
+package io.coodoo.JavaEE7_Angular2_Architype;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class JAXRSConfig extends Application {
+
+}
